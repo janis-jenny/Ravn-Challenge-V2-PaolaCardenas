33b6c7e89be20e59d0fe345a62d13b877131e672
janis-jenny <pcardenasvm@gmail.com>
2021-08-27 18:17:17 -0500

    Add basic structure and call the api in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App" />
-  );
-}
+const client = new ApolloClient({
+  uri: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
+  cache: new InMemoryCache(),
+});
+
+const App = () => (
+  <>
+    <ApolloProvider client={client}>
+      <h1>Characters</h1>
+    </ApolloProvider>
+  </>
+);
 
 export default App;
