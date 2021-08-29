98b4aa33cd16e53a4e007d5375c7f876b37f61bb
janis-jenny <pcardenasvm@gmail.com>
2021-08-29 18:46:10 -0500

    Add react loading animation and style loading and error

--- src/components/common/Error.js
+++ src/components/common/Error.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Error = ({ error }) => <div className="d-flex justify-content-center py-2"><span className="text-danger">{error}</span></div>;
+const Error = ({ error }) => <div className="d-flex justify-content-center py-2"><span className="text-danger fs-3">{error}</span></div>;
 
 Error.defaultProps = {
   error: 'Failed to load data',
--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import ReactLoading from 'react-loading';
 import Navbar from '../Navbar';
 
 const Loading = () => (
   <>
     <Navbar />
     {' '}
-    <div className="d-flex justify-content-center py-3"><span>Loading</span></div>
+    <div className="d-flex justify-content-center py-3">
+      <ReactLoading type="spokes" color="#333333" height={48} width={32} />
+      <span className="fs-3 p-2">Loading</span>
+    </div>
   </>
 );
 
