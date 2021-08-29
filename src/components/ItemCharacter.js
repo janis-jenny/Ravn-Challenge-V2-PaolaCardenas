ad8af90269bf9b90f8dff99fdf3caee9a18dd8b9
janis-jenny <pcardenasvm@gmail.com>
2021-08-29 15:17:41 -0500

    Add Link in ItemCharacter to go to the carachter page with more info

--- src/components/ItemCharacter.js
+++ src/components/ItemCharacter.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const ItemCharacter = ({ id, name, origin }) => (
-  <div id={id} className="card border-bottom border-light mb-0">
-    <div className="card-body">
-      <h4 className="card-title">{name}</h4>
-      <p className="card-text">{origin}</p>
+  <Link to={`/character/${id}`}>
+    <div id={id} className="card border-bottom border-light mb-0">
+      <div className="card-body">
+        <h4 className="card-title">{name}</h4>
+        <p className="card-text">{origin}</p>
+      </div>
+      <div className="arrow"><i className="fas fa-chevron-right fa-2x" /></div>
     </div>
-    <div className="arrow"><i className="fas fa-chevron-right fa-2x" /></div>
-  </div>
+  </Link>
 );
 
 ItemCharacter.propTypes = {
